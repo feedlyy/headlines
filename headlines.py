b1223526f8c28d704e73683c930322c682165409
feedlyy <muhammadnurfadlifadli@gmail.com>
2017-11-23 02:16:21 +0700

    with ninja templates

--- headlines.py
+++ headlines.py
@@ -5,7 +5,7 @@ Created on Wed Nov 22 23:47:28 2017
 @author: Feedlyy
 """
 
-
+from flask import render_template
 
 
 #app = Flask(__name__)
@@ -29,6 +29,8 @@ RSS_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             'cnn': 'http://rss.cnn.com/rss/edition.rss',
             'fox': 'http://feeds.foxnews.com/foxnews/latest'}
 
+
+@app.route("/")
 @app.route("/<publication>")
 
 
@@ -38,14 +40,7 @@ RSS_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEED[publication])
     first_article = feed['entries'][0]
-    return """<html>
-        <body>
-            <h1> HEADLINES </h1>
-            <b>{0}</b> <br/>
-            <i>{1}</b> <br/>
-            <p>{2}</p> <br/>
-        </body>
-    </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+    return render_template("home.html", articles = feed['entries'])
     
 if __name__ == "__main__":
     app.run(port=5010, debug=True)
